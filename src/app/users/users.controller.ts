93e230f3b9796ec5688a7d33b743ba59b59d159f
João Pedro <nmjoaopedro22@gmail.com>
2026-04-04 13:35:13 -0300

    feat: adiciona swagger para docs

--- src/app/users/users.controller.ts
+++ src/app/users/users.controller.ts
@@ -1,15 +1,18 @@
 import { Body, Controller, Post } from "@nestjs/common";
+import { ApiOperation, ApiTags } from "@nestjs/swagger";
 import { ZodResponse } from "nestjs-zod";
 
 import { CreateUserResponse } from "./responses/create-user.response";
 import { CreateUserSchema } from "./schemas/create-user.schema";
 import { UsersService } from "./users.service";
 
+@ApiTags("Users")
 @Controller("users")
 export class UsersController {
 	constructor(private readonly service: UsersService) {}
 
 	@Post()
+	@ApiOperation({ summary: "Cria um novo usuário" })
 	@ZodResponse({ type: CreateUserResponse })
 	async createUser(@Body() body: CreateUserSchema) {
 		const user = await this.service.createUser(body);
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,10 @@ import { VersioningType } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { NestFactory } from "@nestjs/core";
 import { NestExpressApplication } from "@nestjs/platform-express";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import compression from "compression";
 import helmet from "helmet";
+import { cleanupOpenApiDoc } from "nestjs-zod";
 
 import { AppModule } from "./app.module";
 
@@ -20,6 +22,33 @@ async function bootstrap() {
 	app.enableCors({
 		origin: "*",
 	});
+
+	const config = new DocumentBuilder()
+		.setTitle("Teste Starti")
+		.setVersion("1.0")
+		.addBearerAuth({ bearerFormat: "JWT", scheme: "bearer", type: "http" })
+		.build();
+
+	const document = SwaggerModule.createDocument(app, config, {
+		operationIdFactory: (
+			controllerKey: string,
+			methodKey: string,
+			version?: string,
+		) => {
+			const controller = controllerKey
+				.replace(/Controller$/, "")
+				.toLowerCase()
+				.split("-")
+				.map((word, index) =>
+					index === 0
+						? word
+						: word.charAt(0).toUpperCase() + word.slice(1),
+				)
+				.join("");
+			return `${controller}_${methodKey}_${version}`;
+		},
+	});
+	SwaggerModule.setup("swagger", app, cleanupOpenApiDoc(document));
 
 	await app.listen(configService.getOrThrow<number>("PORT"));
 }
